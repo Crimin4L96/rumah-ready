1f05bfe4eb7e16064822f2137051d9f75aae0007
Ahmad Arif Saputra <ahmadarifsaputra@gmail.com>
2026-05-12 12:12:38 +0700

    [backend] : Add servis get all product

--- apps/backend/src/routes/product.ts
+++ apps/backend/src/routes/product.ts
@@ -3,6 +3,7 @@ import { prisma } from "../lib/prisma";
 
 const router = express.Router();
 
+// create product
 router.post("/", async (req, res) => {
   try {
     const { name, price } = req.body;
@@ -16,6 +17,23 @@ router.post("/", async (req, res) => {
       });
 
     res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+});
+
+// get all product
+router.get("/", async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json({
       error: "Internal server error",
