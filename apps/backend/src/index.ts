2223b549118a82d815ff0ad9b69143d331cfc460
Ahmad Arif <ahmadarif@Ahmad-Arif-Mac.local>
2026-05-13 12:41:12 +0700

    init

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://https://rumah-ready-frontend.vercel.app"
+    "https://rumah-ready-frontend.vercel.app"
   ]
 }));
 
